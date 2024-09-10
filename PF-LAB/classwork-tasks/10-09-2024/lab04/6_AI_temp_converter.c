ee3fd2de351f46505f74b57aeb5dbb793c77f0f4
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-09-10 22:27:20 +0500

    commenting

--- PF-LAB/classwork-tasks/10-09-2024/lab04/6_AI_temp_converter.c
+++ PF-LAB/classwork-tasks/10-09-2024/lab04/6_AI_temp_converter.c
@@ -1,37 +1,45 @@
-//start
+//----------------------------------------------- START -----------------------------------------------
 #include <stdio.h>
 
 int main (){
-    //declaration
+
+    //------------------------------------------------- DECLARATION -------------------------------------------------
     char init_temp_unit;
     char fin_temp_unit;
     float init_temp_val;
     float fin_temp_val;
 
-    // input
+    //----------------------------------------------- INPUT -----------------------------------------------
+
+    // - first unit
     printf("Enter your temperature unit: ");
     scanf("%c",&init_temp_unit);
 
+    // - to get correct unit 
     while (init_temp_unit != 'C' && init_temp_unit != 'F' &&  init_temp_unit != 'K')
     {
         printf("Enter your temperature unit(C,F,K): ");
         scanf("%c",&init_temp_unit);
     }
     
-
+    // first unit temperature value
     printf("Enter your temperature value: ");
     scanf("%f",&init_temp_val);
 
+
+    // - second unit
     printf("Enter temperature unit to convert to: ");
     scanf("%c",&fin_temp_unit);
 
+
+    // - to get correct unit 
     while (fin_temp_unit != 'C' && fin_temp_unit != 'F' && fin_temp_unit != 'K') // only run loop when input is neither C,F,K
     {
         printf("Enter temperature unit to convert to(C,F,K): ");
         scanf("%c",&fin_temp_unit);
     }
 
-    // processing
+    //------------------------------------------------- PROCESSING -------------------------------------------------
 
     // - C to F
     if (init_temp_unit == 'C' && fin_temp_unit == 'F'){
@@ -59,7 +67,9 @@ int main (){
     }
 
     printf("your result: %f",fin_temp_val);
+    printf("Thank you for using our AI-powered temperature converter.");
 
     return 0;
 
 }
+//------------------------------------------------- END -------------------------------------------------
