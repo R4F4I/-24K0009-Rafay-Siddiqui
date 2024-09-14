add256ecd007106a21e4ef78a039baf22a14fe0d
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-09-14 08:42:07 +0500

    final exercise

--- PF-LAB/homework-tasks/LAB04/6_online_store.c
+++ PF-LAB/homework-tasks/LAB04/6_online_store.c
@@ -8,3 +8,47 @@ Print the actual amount, saved amount and the amount after discount.
 The Minimum amount eligible for a discount is 500.
 */
 
+#include <stdio.h>
+
+int main(){
+    float items_cost;
+    float saved_cost;
+    float final_cost;
+    int discount;
+
+    //input
+    printf("Enter the cost of items: ");
+    scanf("%f",&items_cost);
+
+    //criteria for discount
+    if (items_cost<500) //less than 500
+    {
+        discount = 0;
+    } 
+    else if (items_cost>500 && items_cost<2000) // b/w 500 & 2000
+    {
+        discount = 5;
+    } 
+    else if (items_cost>2000 && items_cost<4000) // b/w 2000 & 4000
+    {
+        discount = 10;
+    }
+    else if (items_cost>4000 && items_cost<6000) // b/w 4000 & 6000
+    {
+        discount = 20;
+    }
+    else if (items_cost>6000) // more than 6000
+    {
+        discount = 35;
+    }
+    
+    saved_cost = items_cost*discount/100;
+    final_cost = items_cost - saved_cost;
+
+    printf("\ndiscount: %d",discount);
+    printf("\nsaved cost: %.2f",saved_cost);
+    printf("\nfinal cost: %.2f",final_cost);
+    
+
+    return 0;
+}
