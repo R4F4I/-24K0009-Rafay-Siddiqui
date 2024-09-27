4175488c3e5041ad468c4a411c7d1124202ccd7c
R4F4I <94185789+R4F4I@users.noreply.github.com>
2024-09-27 08:32:15 +0500

    section 2 prob1

---
+++ PF-LAB/homework-tasks/LAB05/section 2/prob1.c
@@ -0,0 +1,15 @@
+// ? Write a program that checks if a number is divisible by both 3 and 5 using logical operators.
+
+#include <stdio.h>
+
+int main (){
+
+    int num;
+
+    if (num % 3 == 0 && num % 5 == 0){
+        printf("num is divisble by both");
+    }
+
+    return 0;
+
+}
