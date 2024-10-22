816af6aa1919d083b86182faf432810b73a7f90e
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-10-22 20:50:24 +0500

    successfully completed centered hollowed triangle

---
+++ PF-Theory/rand_excersises/centered_hollow_triangle.c
@@ -0,0 +1,68 @@
+/*
+
+......*
+....*   *
+..*       *
+* * * * * * *
+
+Enter an odd num: 19
+                   *
+                  * *
+                 *   *
+                *     *
+               *       * 
+              *         *
+             *           *
+            *             *
+           *               * 
+          *                 *
+         *                   *
+        *                     *
+       *                       *
+      *                         * 
+     *                           *
+    *                             *
+   *                               *
+  *                                 * 
+ *                                   *
+* * * * * * * * * * * * * * * * * * * *
+
+
+*/
+
+#include <stdio.h>
+
+
+int main(){
+
+    int n=0;
+    int i,j;
+
+    while (n%2==0)
+    {
+        printf("Enter an odd num: ");
+        scanf("%d",&n);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        // outer spaces
+        for (j = 0; j <n-i ; j++){printf(" ");}
+
+        // 1st *
+        printf("* ");
+
+        // inner spaces
+        if (i>0){for (j = 1; j < ((2*i)-1); j++){printf(" ");}}
+
+        // 2nd *
+        if (i!=0){printf("* ");}
+        
+        // newline
+        printf("\n");
+    }
+
+    for (i = 0; i <= n; i++){printf("* ");}
+
+    return 0;
+}
