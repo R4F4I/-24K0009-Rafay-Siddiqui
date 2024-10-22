cbaf91280178a1d60bc71405654c13c8a57db9ec
R4F4I <94185789+R4F4I@users.noreply.github.com>
2024-10-22 11:16:38 +0500

    correct spiral

---
+++ PF-Theory/rand_excersises/sprail_correct.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void print_arr(int arr[20][20]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int lim = 0;
+    while (lim % 2 == 0) {
+        printf("Enter an odd number: ");
+        scanf("%d", &lim);
+    }
+
+    int arr[20][20] = {0};
+    int n = lim;
+    int row = n / 2, col = n / 2;  // Start from the middle
+    int i = 1;
+
+    // Directions for traversal: right, down, left, up
+    int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int d = 0;  // Start direction (right)
+
+    for (int step = 1; step <= n; step++) {
+        for (int j = 0; j < step; j++) {
+            arr[row][col] = i++;
+            col += dir[d][1];  // Move in current direction
+        }
+        d = (d + 1) % 4;  // Change direction
+
+        for (int j = 0; j < step; j++) {
+            arr[row][col] = i++;
+            row += dir[d][0];  // Move in current direction
+        }
+        d = (d + 1) % 4;  // Change direction
+    }
+
+    // Fill in the remaining middle value if needed
+    arr[row][col] = i;
+
+    print_arr(arr);
+    return 0;
+}
