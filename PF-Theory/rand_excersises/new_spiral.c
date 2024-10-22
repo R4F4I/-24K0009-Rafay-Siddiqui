5d65945c3f4a85966bbe103d1abc7692c142926f
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-10-22 22:03:03 +0500

    starting anew

---
+++ PF-Theory/rand_excersises/new_spiral.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main (){
+
+    int dir[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+
+
+
+    return 0;
+}
--- PF-Theory/rand_excersises/sprail_correct.c
+++ PF-Theory/rand_excersises/sprail_correct.c
@@ -3,7 +3,7 @@
 void print_arr(int arr[20][20]) {
     for (int i = 0; i < 7; i++) {
         for (int j = 0; j < 7; j++) {
-            printf("%d ", arr[i][j]);
+            printf("%.2d ", arr[i][j]);
         }
         printf("\n");
     }
@@ -19,18 +19,23 @@ int main() {
     int arr[20][20] = {0};
     int n = lim;
     int row = n / 2, col = n / 2;  // Start from the middle
-    int i = 1;
+    int i = 1,j=n*n;
 
     // Directions for traversal: right, down, left, up
-    int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int dir[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
     int d = 0;  // Start direction (right)
 
     for (int step = 1; step <= n; step++) {
+        if (step == i*i)
+        {
+            break;
+        }
+        
         for (int j = 0; j < step; j++) {
             arr[row][col] = i++;
             col += dir[d][1];  // Move in current direction
         }
-        d = (d + 1) % 4;  // Change direction
+        d = (d + 1) % 4;  // Change direction 
 
         for (int j = 0; j < step; j++) {
             arr[row][col] = i++;
