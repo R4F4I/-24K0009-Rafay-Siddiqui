e8ec2336c3e36f1c9780e006a85b49277625cba2
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-12-02 18:13:02 +0500

    q6

--- PF-LAB/homework-tasks/LAB12/q6.c
+++ PF-LAB/homework-tasks/LAB12/q6.c
@@ -14,19 +14,114 @@
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
 
-#define FILENAME "file.txt"
+# define FILENAME "file.txt"
+# define BUFF_SIZE 15
+
+struct word_freq
+{
+    char word[10];
+    int freq;
+};
+
+
+void word_freq(char sentence[]){
+    int i=0,j=0,space=0;
+    char word[20];
+
+    // approximate words in sentence
+    while (sentence[i]!='\0')
+    {
+        if (sentence[i]== ' ')space++;
+        i++;
+    }
+    // printf("space: %d",space);
+
+
+    // init arr of type struct of size 'space'
+    struct word_freq *arr = calloc(space,sizeof(struct word_freq));
+    
+    for (i = 0; i < space; i++)
+    {
+        // move to the last space in arr
+        j=0;
+        while (arr[j].freq!=0)
+        {
+           j++;
+        }
+        // form word from sentence
+        int k=0;
+        while(sentence[k]!=' ' && sentence[k]!='\0')
+        {
+            word[k]=sentence[k];
+            k++;
+        }
+        arr[j].word[k]='\0';
+        arr[j].freq++;
+        k++;
+        
+        
+        // printf("%d ",arr[i].freq);
+    }
+    
+
+}
 
 int main(){
-
+    int i=0;
     FILE *fp;
-    fp = fopen(FILENAME,"w");
-
-    // will hold all the unique words
-    void *buffer = NULL;
+    fp = fopen(FILENAME,"r");
 
 
- return 0;
+    int buffer_size = BUFF_SIZE;
+    // will hold words
+    char *buffer   = malloc(buffer_size*sizeof(char));
+    
+    // used calloc to initialise values in the allocated mem block to prevent garbage values
+    char *sentence = calloc(buffer_size,sizeof(char));
+    char *temp = malloc(buffer_size*sizeof(char));
+
+
+
+    while (fscanf(fp,"%s",buffer)!=EOF)
+    {
+        // buffer resize
+        if (strlen(sentence)+strlen(buffer)+2>buffer_size)
+        {
+            buffer_size *=2;
+            // realloc returns a pointer to the reallocated memory block
+            char *temp = realloc(sentence,buffer_size*sizeof(char));
+            // error handling
+            if (temp == NULL) {
+                printf("Failed to reallocate memory\n");
+                free(buffer);
+                free(sentence);
+                fclose(fp);
+                return 1;
+            }
+            
+            // update the sentence pointer
+            sentence = temp;
+        }
+
+        strcat(sentence,buffer);
+        strcat(sentence," ");
+    }
+
+
+    printf("%s ",sentence);
+    printf("\nsuccessfully read file...\n");
+
+
+    word_freq(sentence);
+
+
+    free(buffer);
+    free(sentence);
+    fclose(fp);
+
+    return 0;
 }
